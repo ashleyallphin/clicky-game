25c0b7e013e5d8273e7cc4ce5bc8d5b36a3e9f9f
Ashley Allphin <ashley.allphin@gmail.com>
2020-06-30 18:57:57 -0400

    Updates

---
+++ precache-manifest.4cdcb9b142e66cabdfd39b508112b5ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47b3a5d77f90b4b222ec9213d5a855c2",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "41e00849b6dd76d79f0b",
+    "url": "/clicky-game/static/css/main.6b218a25.chunk.css"
+  },
+  {
+    "revision": "146e68e74478ff587202",
+    "url": "/clicky-game/static/js/2.0ffa3ab3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/clicky-game/static/js/2.0ffa3ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41e00849b6dd76d79f0b",
+    "url": "/clicky-game/static/js/main.9c4999ac.chunk.js"
+  },
+  {
+    "revision": "ea8de61c71fd36f495fc",
+    "url": "/clicky-game/static/js/runtime-main.596060ab.js"
+  },
+  {
+    "revision": "dc52114988b0c7e1ba4f8d389e8c3673",
+    "url": "/clicky-game/static/media/boosh-monkey-head.dc521149.png"
+  },
+  {
+    "revision": "1f673426dd9016816a5db4ee65f24059",
+    "url": "/clicky-game/static/media/github-logo.1f673426.svg"
+  }
+]);
